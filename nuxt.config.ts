a6ddbf6557da256424f9233a724da1fcdd9f18d1
Alaaeldeen Hamed <alaaeldeen.hamed@aydi.com>
2026-02-23 00:10:48 +0200

    chore: adding vueuse to our project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: ['@vueuse/nuxt'],
   css: ['~/assets/css/main.css'],
   app: {
     head: {
